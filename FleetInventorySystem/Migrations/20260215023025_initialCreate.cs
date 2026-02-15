fcd2aa9ac2c8b5ebd6a6d684186c4c86cf1cb4fe
drewb8L <104442990+drewb8L@users.noreply.github.com>
2026-02-14 21:13:51 -0700

    feat:(Infrastructure) (#4)
    - add EF Core and Npgsql packages
    - add UserContext
    - add initial migration
    - update docker-compose & gitignore for db

---
+++ FleetInventorySystem/Migrations/20260215023025_initialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fleet_Inventory_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    tech_number = table.Column<string>(type: "text", nullable: false),
+                    employee_number = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
